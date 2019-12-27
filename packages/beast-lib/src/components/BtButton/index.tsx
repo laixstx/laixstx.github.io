86d2485b03ec2c9ff2f5f897ce3cad78379c9fd5
laixstx <18826408945@163.com>
2019-12-27 19:27:05 +0800

    add beast-lib after remove submodule

---
+++ packages/beast-lib/src/components/BtButton/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Button} from 'antd';
+import {ButtonProps} from 'antd/es/button'
+
+/**
+ * 个性化的属性
+ */
+export interface BtButtonProps {
+    // /**
+    //  * 你的个性化属性
+    //  */
+    // tt?: string
+}
+
+export default class BtButton extends React.Component<BtButtonProps & ButtonProps> {
+
+    static Group = Button.Group;
+
+    render(): React.ReactNode {
+        return <Button {...this.props}/>;
+    }
+}
+
+export class BtButton_ extends React.Component<BtButtonProps> {
+    render(): React.ReactNode {
+        return null;
+    }
+}
